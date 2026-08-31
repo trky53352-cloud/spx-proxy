34a2f617f11a379467370c15be56289f26affe88
trky53352-cloud <trky53352@gmail.com>
2026-08-31 11:26:26 +0300

    Create spx-chain.js

---
+++ api/spx-chain.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
+  const TOKEN = process.env.MARKETDATA_TOKEN;
+  const underlying = req.query.underlying || "SPX";
+
+  try {
+    const url = `https://api.marketdata.app/v1/options/chain/${underlying}/?token=${TOKEN}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ s: "error", errmsg: err.message });
+  }
+}
