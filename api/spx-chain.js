8022f7baee99ba1b78a16ead2e9fa22785a229c1
trky53352-cloud <trky53352@gmail.com>
2026-09-01 11:59:48 +0300

    Update spx-chain.js

--- api/spx-chain.js
+++ api/spx-chain.js
@@ -2,11 +2,11 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
 
-  const TOKEN = process.env.MARKETDATA_TOKEN;
+  const TOKEN = "OFBBWEVfdkM4cHlxS0N6cm9GUzhyYWtVVlZVLUxNb1d6QUYtWmlLTW1nbz0";
   const underlying = req.query.underlying || "SPX";
 
   try {
-    const url = `https://api.marketdata.app/v1/options/chain/${underlying}/?token=${TOKEN}`;
+    const url = "https://api.marketdata.app/v1/options/chain/" + underlying + "/?token=" + TOKEN;
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
